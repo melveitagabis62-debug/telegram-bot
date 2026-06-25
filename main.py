0f476a468b8eec296a5ae1ab3f315441a08a4d8b
melveitagabis62-debug <melveitagabis62@gmail.com>
2026-06-25 20:14:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ async def start_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-        await query.answer()
+    await query.answer()
 
     data = query.data
 
