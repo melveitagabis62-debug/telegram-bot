a3119dd57ccee97e56bdf001956d70edbc89bf9a
melveitagabis62-debug <melveitagabis62@gmail.com>
2026-07-02 08:19:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -294,6 +294,7 @@ app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(handle_buttons))
-app.add_handler(MessageHandler(filters.TEXT & \~filters.COMMAND, handle_text))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+
 
 app.run_polling()
