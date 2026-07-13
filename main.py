48bbb49b98a11ad73d070a622ff73303f0922265
melveitagabis62-debug <melveitagabis62@gmail.com>
2026-07-13 17:53:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -258,4 +258,5 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(handle_buttons))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
+
 app.run_polling()
